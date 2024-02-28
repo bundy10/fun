06e06c05a7908ab74cb59719c7643606ec93e74a
bundy huang <bundy.huang@myob.com>
2024-02-29 03:48:24 +1100

    summation last test

--- summation.js
+++ summation.js
@@ -1,11 +1,5 @@
 function summation(num) {
-    let result = 0;
-    let idx = 0;
-    while (idx < num + 1){
-        result += idx;
-        idx += 1;
-    }
-    return result;
+    return (num * (num + 1)/2)
 }
 
 module.exports = summation;
