79455477516c2cc09323c9a1c63565e13a07ac8f
bundy huang <bundy.huang@myob.com>
2024-02-29 05:04:16 +1100

    final MultiplesOfThreeAndFive Kata

--- Test/multiplesOfThreeAndFive.test.js
+++ Test/multiplesOfThreeAndFive.test.js
@@ -15,9 +15,28 @@ describe('multiplesOfThreeAndFive', () => {
         assert.deepStrictEqual(result, expectedResult)
     });
     it('shall return positive number if input is a positive number', () => {
-        const numInput = 1
-        const expectedResult = 1
+        const numInput = 3
+        const expectedResult = 0
         const result = multiplesOfThreeAndFive(numInput);
         assert.deepStrictEqual(result, expectedResult)
     });
+    it('shall return sum of 1 to n', () => {
+        const numInput = 0
+        const expectedResult = 0
+        const result = multiplesOfThreeAndFive(numInput);
+        assert.deepStrictEqual(result, expectedResult)
+    });
+    it('shall return sum of 1 to n that are multiples of 3', () => {
+        const numInput = 4
+        const expectedResult = 3
+        const result = multiplesOfThreeAndFive(numInput);
+        assert.deepStrictEqual(result, expectedResult)
+    });
+    it('shall return sum of 1 to n that are multiples of 3 and 5', () => {
+        const numInput = 10
+        const expectedResult = 23
+        const result = multiplesOfThreeAndFive(numInput);
+        assert.deepStrictEqual(result, expectedResult)
+    });
+
 });
--- multiplesOfThreeAndFive.js
+++ multiplesOfThreeAndFive.js
@@ -1,6 +1,19 @@
 
-function multiplesOfThreeAndFive(num) {
-    return num <= 0 ? 0 : num
+
+function multiplesOfThreeFinder(num)  {
+    return num % 3 === 0 || num % 5 === 0 ? num : 0
 }
+function multiplesOfThreeAndFive(num) {
+    if (num <= 1){
+        return 0
+    }
+
+    const copy = num - 1
+    const arrayOfCopy = Array.from({length: copy}, (_, index) => multiplesOfThreeFinder(index + 1))
+    console.log(arrayOfCopy)
+    return arrayOfCopy.reduce((acc, curr) => acc + curr)
+}
+
+
 
 module.exports = multiplesOfThreeAndFive;
